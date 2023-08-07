cd17b979043f54b25e5390a17dedd5734985a261
kohei3110 <kk.31108424@gmail.com>
2023-08-07 21:52:28 +0900

    put default values in example code

--- az_oracle_automation/terraform/examples/installation_basic/main.tf
+++ az_oracle_automation/terraform/examples/installation_basic/main.tf
@@ -1,11 +1,36 @@
+resource "azurerm_resource_group" "basic" {
+  location = var.location
+  name     = "rg-oracle-automation-basic"
+}
+
 module "virtual_network" {
   source = "../../modules/virtual_network"
+
+  resource_group_name  = azurerm_resource_group.basic.name
+  address_space_vnet   = ["10.0.0.0/16"]
+  address_space_subnet = ["10.0.1.0/24"]
+  virtual_network_name = "ora-vnet-basic"
+
+  depends_on = [azurerm_resource_group.basic]
 }
 
 module "bastion" {
   source = "../../modules/bastion"
+
+  resource_group_name                 = azurerm_resource_group.basic.name
+  virtual_network_name                = module.virtual_network.virtual_network_name
+  azure_bastion_service_name          = "ora-bastion-basic"
+  azure_bastion_subnet_address_prefix = ["10.0.0.0/24"]
+
+  depends_on = [azurerm_resource_group.basic, module.virtual_network]
 }
 
 module "virtual_machine" {
   source = "../../modules/virtual_machine"
+
+  resource_group_name  = azurerm_resource_group.basic.name
+  virtual_network_name = module.virtual_network.virtual_network_name
+  oravm_admin_password = var.oravm_admin_password
+
+  depends_on = [azurerm_resource_group.basic, module.virtual_network]
 }
--- az_oracle_automation/terraform/examples/installation_basic/variables.tf
+++ az_oracle_automation/terraform/examples/installation_basic/variables.tf
@@ -1,11 +1,9 @@
-module "virtual_network" {
-  source = "../../modules/virtual_network"
+variable "oravm_admin_password" {
+  description = "The Password which should be used for the local-administrator on this Virtual Machine"
+  default     = null
 }
 
-module "bastion" {
-  source = "../../modules/bastion"
-}
-
-module "virtual_machine" {
-  source = "../../modules/virtual_machine"
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created"
+  default     = "eastus"
 }
