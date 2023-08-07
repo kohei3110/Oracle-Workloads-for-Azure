8ee3cbf786eebe124e5733a34c10cb8b3520191f
kohei3110 <kk.31108424@gmail.com>
2023-08-07 21:41:55 +0900

    remove extra output values

--- az_oracle_automation/terraform/modules/virtual_machine/outputs.tf
+++ az_oracle_automation/terraform/modules/virtual_machine/outputs.tf
@@ -1,10 +1,1 @@
-output "ora_vm_password" {
-  description = "Password for the Oracle Virtual Machine"
-  sensitive   = true
-  value       = var.oravm_admin_password
-}
 
-output "linux_vm_private_ips" {
-  description = "Private IP's map for the all linux Virtual Machines"
-  value       = zipmap(azurerm_linux_virtual_machine.linux_vm.*.name, azurerm_linux_virtual_machine.linux_vm.*.private_ip_address)
-}
--- az_oracle_automation/terraform/modules/virtual_network/outputs.tf
+++ az_oracle_automation/terraform/modules/virtual_network/outputs.tf
@@ -1,9 +1,4 @@
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.address_space
-}
-
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = local.azurerm_subnets[*].id
+output "virtual_network_name" {
+  description = "The Name of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.name
 }
